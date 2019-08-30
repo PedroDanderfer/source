09b95845fb00361e6fb7c66224770a77eeed0238
Pedrito <piterstone5@gmail.com>
2019-08-30 09:26:56 -0300

    Register y login hechos, falta session

--- src/database.js
+++ src/database.js
@@ -1,20 +1,28 @@
 const mongoose = require("mongoose"),
-      Schema = mongoose.Schema;
+      Schema = mongoose.Schema,
+      db = mongoose.connection,
+      uniqueValidator = require('mongoose-unique-validator');;
 
-mongoose.createConnection("mongodb://localhost/userAccounts");
+db.on('error', console.error.bind(console, ("Connection error")));
+db.once('open', function(){
+    console.log("Connected");
+});
+
+mongoose.connect("mongodb://localhost/vRacingArgentina", {useNewUrlParser: true});
 
 var user_Schema = new Schema({
-    email : {type : String, required : true},
-    password : {
-                    type : String, 
-                    required : true,
-                    validate : {validator:function(p){
-                                    return this.password_confirmation == p;
-                                },
-                                message:"Las contraseñas no coinciden"}, 
-                    minlength : [8, "La contraseña debe tener mas de 8 caracteres"]
-    }
+    email : {type : String, required : true, unique : true},
+    name : {type : String, required : true, unique : true},
+    password : {type : String, 
+                required : true,
+                validate : {validator:function(p){
+                                return this.password_confirmation == p;
+                            },
+                message:"Las contraseñas no coinciden"}, 
+                minlength : [8, "La contraseña debe tener mas de 8 caracteres"]}
 });
+
+user_Schema.plugin(uniqueValidator);
 
 user_Schema.virtual("password_confirmation").get(function(){
     return this.p_c;
@@ -22,6 +30,6 @@ user_Schema.virtual("password_confirmation").get(function(){
     this.p_c = userPassword;
 });
 
-var userSchema = mongoose.model("Users", user_Schema);
+var userSchema = mongoose.model("Users", user_Schema,"userAccounts");
 
 module.exports.userCreate = userSchema;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express"),
       app = express(),
       path = require("path"),
-      bodyParser = require("body-parser")
+      bodyParser = require("body-parser"),
+      session = require("express-session");
       ;
 
 //DATABASE
@@ -26,19 +27,20 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req,res){
+
     var user = new User({
         email: req.body.userEmail,
+        name : req.body.userName,
         password: req.body.userPassword,
         password_confirmation: req.body.password_confirmation
     });
-    console.log(user.password_confirmation);
-    user.save(function(err){
-        if(err){
-            console.log(String(err));
-        }else{
-            console.log("Datos guardados");
-        }
+
+
+    user.save(function(err, user){
+        if(err){ return console.error(err);}
+        console.log(user);
     });
+
 });
 
 //login
@@ -47,6 +49,11 @@ app.get("/login", function(req,res){
     res.sendFile(path.join(__dirname,"/public/html/login.html"));
 });
 
+app.post("/login", function(req,res){
+    User.findOne({email : req.body.email, password : req.body.password},function(err, User){
+        console.log(User);
+    });
+});
 
 
 //LISTEN
